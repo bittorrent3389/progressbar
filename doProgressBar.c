b136a205dc30224ace0ffac45baae7990784a541
root <ggla>
2016-03-13 01:04:22 +0800

    second

--- doProgressBar.c
+++ doProgressBar.c
@@ -2,10 +2,10 @@
 
 static inline void doProgess( const char *string, int step, int total ) {
 	int width = 50;
-	float percent = ( step *  100 ) / total;
+	float percent = ( (float)step *  100 ) / (float)total;
 	int c =(int) ( (percent / 100) * (float) width);
 	printf("%s ", string);
-	printf(" %3d%% [", (int)percent);
+	printf(" %3.2f%% [", percent);
 	//load show bar
 	int x;
 	for (x = 0; x<c; x++) printf("=");
@@ -17,14 +17,20 @@ static inline void doProgess( const char *string, int step, int total ) {
 
 
 void main() {
-	
-	int total = 2;
+	printf("%ld \n", (unsigned long)0x10);
+	//int total = 40;
 	int step = 0;
-
+	int i;
+	for ( i = 0; i < 0x10 ; i++ ) {
+		sleep(1);
+		doProgess( "Progess", i+1, (int) 0x10);
+	}
+	/*
 	while ( step < total ) {
 		sleep(1);
 		step++;
 		doProgess( "Progess :", step, total );
 	}
+	*/
 	puts("\nover");
 }
